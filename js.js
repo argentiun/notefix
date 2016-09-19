bb56b55fd6f1fa0c03d0d34966d7564da9cd7966
KeviinMiichel <ghiokevin@gmail.com>
2016-09-19 08:24:52 -0300

    Aca subo JS de FAQ y form falta AJAX y boostrap

--- js.js
+++ js.js
@@ -1,81 +1,85 @@
 window.onload = function (){
 
-    var nombre = document.getElementById("register")[0]
-    var apellido = document.getElementById("register")[1]
-    var tel =  document.getElementById("register")[2]
-    var email = document.getElementById("register")[3]
-    var pass1 = document.getElementById("register")[4]
-    var pass2 = document.getElementById("register")[5]
-    var nombreapellido = []
-    nombreapellido.push(nombre+apellido)
+    var name = document.getElementById("name");
+    var lastname = document.getElementById("lastname");
+    var tel =  document.getElementById("tel");
+    var email = document.getElementById("email");
+    var pass1 = document.getElementById("pass1");
+    var pass2 = document.getElementById("pass2");
+    var fullname = [];
+    fullname.push(name+lastname);
 
-    var form = document.getElementById("register")
+    var form = document.getElementById("register");
 
     form.addEventListener("submit", function(e){
-      e.preventDefault(e);
+        e.preventDefault(e);
 
-      if (nombre.value.length === 0 ){
-        alert("El campo Nombre Es obligatorio");
-      };
+        if (name.value.length === 0 ){
+          console.log("Completa el nombre !");
+          return false;
+        }
+        if ( !/^[a-zA-Z()]*$/.test(name.value) ) {
+          console.log("Error: El nombre no es válido: " + name.value);
+          return false;
+        }
 
-      console.log(nombre.value);
-
-      if (apellido.value.length === 0 ){
-        alert("El campo Apellido Es obligatorio");
-      };
-
-      console.log(apellido.value);
-
-      if (tel.value.length === 0 ){
-        alert("El campo Telefono Es obligatorio");
-      };
-
-      console.log(tel.value);
-
-
-      if (email.value.length === 0 ){
-        alert("El campo Email Es obligatorio");
-      };
-
-      console.log(email.value);
-
-      if (pass1.value.length === 0 ){
-        alert("El campo contraseña Es obligatorio");
-      };
-
-      console.log(pass1.value);
-
-      if (pass2.value.length === 0 ){
-        alert("El campo repetir contraseña Es obligatorio");
-      };
-
-      console.log(pass2.value);
-
-      if (pass1.value.length != pass2.value.length){
-          alert("Las contraseñas no coinciden");
-      }
+        if (lastname.value.length === 0 ){
+          console.log("Completa el apellido !");
+          return false;
+        }
+        if ( !/^[a-zA-Z() ]+$/.test(lastname.value) ) {
+          console.log("Error: El apellido no es válido: " + lastname.value);
+          return false;
+        }
 
 
+        var onlytel = /^\+?\d{1,3}?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d\d\d$/;
+        if (tel.value.length === 0 ){
+          alert("El campo Telefono Es obligatorio");
+        }
+        if (onlytel.test(tel.value) ) {
+          console.log("Error: El telefono no es válido: " + tel.value);
+          return false;
+        }
+
+        if (email.value.length === 0 ){
+          console.log("El campo Email Es obligatorio");
+          return false;
+        }
+
+        onlyemail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if ( onlyemail.test(email) ){
+          console.log("Error: La dirección de correo " + email + " es incorrecta.");
+          return false;
+        }
+
+        if (pass1.value.length === 0){
+          console.log("El campo contraseña Es obligatorio");
+          return false;
+        }
+
+        if ( pass2.value.length === 0 ) {
+          console.log("El campo repetir contraseña Es obligatorio");
+          return false;
+        }
+
+        if (pass1.value.length != pass2.value.length){
+            console.log("Las contraseñas no coinciden");
+            return false;
+        }
+
+        var nuevoUsuario = new XMLHttpRequest();
+        nuevoUsuario.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+              console.log(nuevoUsuario.responseText);
+
+          }
+        };
+
+        nuevoUsuario.open("GET", "https://sprint.digitalhouse.com/nuevoUsuario", true);
+        nuevoUsuario.send();
 
 
-    });
+      });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+};
